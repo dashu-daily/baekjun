dd6b6bd3bcb7dbe37233821f9ca0541a1d54ee15
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-07-08 21:08:54 +0900

    baekjun Q2438, Q2439, Q10871

---
+++ baekjun/src/baekjun/step3/Q10871.java
@@ -0,0 +1,26 @@
+package baekjun.step3;
+
+import java.util.Scanner;
+
+public class Q10871 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		
+		int[] A = new int[N]; 
+		
+		for(int i=0; i<N; i++) {
+			A[i] = sc.nextInt();
+		}
+		
+		for(int j=0; j<N; j++) {
+			if(A[j] < X) {
+				System.out.print(A[j] + " ");
+			}
+		}
+	}
+
+}
