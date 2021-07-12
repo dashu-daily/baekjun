9e86cee15dd695ffaf7458a44e869d24b293443e
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-07-12 22:43:38 +0900

    baekjun Q2577

---
+++ baekjun/src/baekjun/step5/Q2577.java
@@ -0,0 +1,40 @@
+package baekjun.step5;
+
+import java.util.Scanner;
+
+public class Q2577 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[10];
+
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		int num = A * B * C;
+		
+		while(true) {
+			int x = num % 10;
+			num /= 10;
+			
+			for(int i=0; i<10; i++) {
+				if(x == i) arr[i]++;
+			}
+			
+			if(num < 10) {
+				for(int i=0; i<10; i++) {
+					if(num == i) arr[i]++;
+				}
+				break;
+			}
+		}
+		
+		for(int i=0; i<10; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
