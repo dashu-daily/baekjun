beedc46810d4baf572cf980242d9f04ef139cd78
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-07-17 20:12:02 +0900

    baekjun Q4673, Q15596

---
+++ baekjun/src/baekjun/step6/Q15596.java
@@ -0,0 +1,14 @@
+package baekjun.step6;
+
+public class Q15596 {
+
+	public long sum(int[] a) {
+		
+		long ans = 0;
+        
+        for(int i=0; i<a.length; i++) {
+			ans += a[i];
+		}
+        return ans;
+	}
+}
---
+++ baekjun/src/baekjun/step6/Q4673.java
@@ -0,0 +1,30 @@
+package baekjun.step6;
+
+public class Q4673 {
+
+	public static void main(String[] args) {
+		for(int i=1; i<= 10000; i++) {
+			boolean selfNum = true;
+			
+			for(int j=1; j<i; j++) {
+				if(d(j) == i) {
+					selfNum = false;
+					break;
+				}
+			}
+			if(selfNum) System.out.println(i);
+		}
+	}
+	
+	public static int d(int n) {
+		
+		int sum = 0;
+		String str = String.valueOf(n);
+		
+		for(int i=0; i<str.length(); i++) {
+			sum += Integer.parseInt(str.charAt(i) + "");
+		}
+		
+		return n + sum;
+	}
+}
