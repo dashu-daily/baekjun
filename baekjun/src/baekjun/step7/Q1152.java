c3b7ec405c484ca871655d7a010fcbb2cbcb7576
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-07-22 21:43:41 +0900

    baekjun Q1152

---
+++ baekjun/src/baekjun/step7/Q1152.java
@@ -0,0 +1,18 @@
+package baekjun.step7;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Q1152 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		StringTokenizer st = new StringTokenizer(str," ");
+		
+		System.out.println(st.countTokens());
+	}
+}
