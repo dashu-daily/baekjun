585bf4c41ebdbadfcd8669ba833b892e30ebf43a
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-07-28 22:50:35 +0900

    baekjun Q10757

---
+++ baekjun/src/baekjun/step8/Q10757.java
@@ -0,0 +1,17 @@
+package baekjun.step8;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Q10757 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		BigDecimal A = sc.nextBigDecimal();
+		BigDecimal B = sc.nextBigDecimal();
+		
+		System.out.println(A.add(B));
+	}
+}
