5ab530eb6faff6f87cac8a0c89970237b662ba2a
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-08-08 18:09:31 +0900

    baekjun Q1929

---
+++ baekjun/src/baekjun/step9/Q1929.java
@@ -0,0 +1,28 @@
+package baekjun.step9;
+
+import java.util.Scanner;
+
+public class Q1929 {
+
+	public static boolean[] isPrime;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int M = sc.nextInt();
+		int N = sc.nextInt();
+		
+		isPrime = new boolean[N+1];
+		
+		isPrime[1] = true;
+		for(int i=2; i<=Math.sqrt(N+1); i++) {
+			for(int j=i*i; j<N+1; j+=i) {
+				isPrime[j] = true;
+			}
+		}
+		
+		for(int i=M; i<=N; i++) {
+			if(!isPrime[i]) System.out.println(i);
+		}
+	}
+}
