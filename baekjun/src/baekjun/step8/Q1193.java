688dc441e9848b77d98916a99b20eab7f4ad8f90
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-08-11 23:16:55 +0900

    baekjun Q1193(mod)

--- baekjun/src/baekjun/step8/Q1193.java
+++ baekjun/src/baekjun/step8/Q1193.java
@@ -6,29 +6,40 @@ public class Q1193 {
 
 	public static void main(String[] args) {
 		
-		Scanner in = new Scanner(System.in);
-		int X = in.nextInt();
+		Scanner sc = new Scanner(System.in);
+//		int X = in.nextInt();
+//		
+//		int count = 1, sum = 0, cnt_in_row = 0;
+//		
+//		while (sum < X) {
+//			count++;
+//			cnt_in_row = X - sum - 1;
+//			sum += count;
+//		}
+//		
+//		System.out.println(cnt_in_row);
+//		int a = 0, b = 0;
+//		if(count % 2 == 0) {
+//			
+//		}
+		int line = 0; 
+		int cnt = 0; 
+		int n = sc.nextInt();
 		
-		int cross_count = 1, prev_count_sum = 0;
- 
-		while (true) {
-        
-			if (X <= prev_count_sum + cross_count) {	
-				
-				if (cross_count % 2 == 1) {	
-					System.out.print((cross_count - (X - prev_count_sum - 1)) + "/" + (X - prev_count_sum));
-					break;
-				} 
-				
-				else {	
-					System.out.print((X - prev_count_sum) + "/" + (cross_count - (X - prev_count_sum - 1)));
-					break;
-				}
- 
-			} else {
-				prev_count_sum += cross_count;
-				cross_count++;
-			}
+		while(cnt < n) {
+			line++; 
+			cnt = line * (line+1) / 2; 
+		} 
+		
+		if( line % 2 != 0) { 
+			int top = 1+(cnt-n); 
+			int bottom = line - (cnt-n); 
+			System.out.println(top+"/"+bottom); 
+		}else { 
+			int top = line - (cnt - n); 
+			int bottom = 1 + (cnt - n); 
+			System.out.println(top+"/"+bottom); 
 		}
+
 	}
 }
