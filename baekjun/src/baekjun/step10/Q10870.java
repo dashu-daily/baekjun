5cbec8016d63e5ca2cd70e54b79225d7a2100492
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-08-13 22:50:35 +0900

    baekjun Q10870(mod)

--- baekjun/src/baekjun/step10/Q10870.java
+++ baekjun/src/baekjun/step10/Q10870.java
@@ -21,16 +21,29 @@ public class Q10870 {
 //		System.out.println(arr[n]);
 		
 		Scanner in = new Scanner(System.in);
-			int N = in.nextInt();
+		int N = in.nextInt();
 	 
-			int[] fibonacci = new int[N + 1];	
-	 
-			for(int i = 0; i < fibonacci.length; i++) {
-				if(i == 0) fibonacci[0] = 0;
-				else if(i == 1) fibonacci[1] = 1;            
-				else fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-			}
+//		int[] fibonacci = new int[N + 1];	
+// 
+//		for(int i = 0; i < fibonacci.length; i++) {
+//			if(i == 0) fibonacci[0] = 0;
+//			else if(i == 1) fibonacci[1] = 1;            
+//			else fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+//		}
         
-		System.out.println(fibonacci[N]);
+//		System.out.println(fibonacci[N]);
+		
+		System.out.println(f(N));
+	}
+	
+	public static int f(int n) {
+		
+		if(n == 0) {
+			return 0;
+		} else if(n == 1) {
+			return 1;
+		}else {
+			return f(n-1) + f(n-2);
+		}
 	}
 }
