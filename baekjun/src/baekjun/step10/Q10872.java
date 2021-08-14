64dfe635a49c38c3844f7908ae1101633dbc2ebc
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-08-14 21:36:03 +0900

    baekjun Q10872

--- baekjun/src/baekjun/step10/Q10872.java
+++ baekjun/src/baekjun/step10/Q10872.java
@@ -5,14 +5,12 @@ import java.util.Scanner;
 public class Q10872 {
 	
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		System.out.println(f(N));
 	}
 	
 	public static int f(int n) {
-		
 		if(n == 0) {
 			return 1;
 		}else {
