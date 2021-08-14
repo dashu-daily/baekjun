9fcd68225607faaf5eb2a472396035a4e9a274d7
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-08-14 21:14:43 +0900

    baekjun Q10872

---
+++ baekjun/src/baekjun/step10/Q10872.java
@@ -0,0 +1,23 @@
+package baekjun.step10;
+
+import java.util.Scanner;
+
+public class Q10872 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+	 
+		System.out.println(f(N));
+	}
+	
+	public static int f(int n) {
+		
+		if(n == 0) {
+			return 1;
+		}else {
+			return f(n-1) * n;
+		}
+	}
+}
