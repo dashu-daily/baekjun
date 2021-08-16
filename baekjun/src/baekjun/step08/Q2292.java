21f09678d4440476539a917e21021362ede8eeda
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-08-16 22:32:51 +0900

    baekjun Q1011

--- baekjun/src/baekjun/step08/Q2292.java
+++ baekjun/src/baekjun/step08/Q2292.java
@@ -8,17 +8,17 @@ public class Q2292 {
 		Scanner in = new Scanner(System.in);
 		 
 		int N = in.nextInt();
-		int count = 1; // 겹 수(최소 루트)
-		int range = 2;	// 범위 (최솟값 기준) 
+		int count = 1; 
+		int range = 2;	
  
 		if (N == 1) {
 			System.out.print(1);
 		}
  
 		else {
-			while (range <= N) {	// 범위가 N보다 커지기 직전까지 반복 
-				range = range + (6 * count);	// 다음 범위의 최솟값으로 초기화 
-				count++;	// count 1 증가 
+			while (range <= N) {	
+				range = range + (6 * count);	
+				count++;	
 			}
 			System.out.print(count);
 		}
