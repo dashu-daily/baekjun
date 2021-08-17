1588c986c4643d808732e6c4635acebb5f6f707d
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-08-17 23:09:29 +0900

    baekjun Q2331

---
+++ baekjun/src/baekjun/step11/Q2331.java
@@ -0,0 +1,34 @@
+package baekjun.step11;
+
+import java.util.Scanner;
+
+public class Q2331 {
+	
+	public static void main(String[] args) {
+		 
+		Scanner in = new Scanner(System.in);
+        
+		int N = in.nextInt();
+        
+		int result = 0;
+ 
+		
+		for(int i = 0; i < N; i++) {
+			int number = i;
+			int sum = 0;	
+			
+			while(number != 0) {
+				sum += number % 10;	
+				number /= 10;
+			}
+			
+			if(sum + i == N) {
+				result = i;
+				break;
+			}
+			
+		}
+ 
+		System.out.println(result);
+	}
+}
