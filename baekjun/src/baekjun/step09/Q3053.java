87dfd063c0099f9f4d08448aabe945e3a8241bb1
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-08-21 23:03:41 +0900

    baekjun Q3053

---
+++ baekjun/src/baekjun/step09/Q3053.java
@@ -0,0 +1,17 @@
+package baekjun.step09;
+
+import java.util.Scanner;
+
+public class Q3053 {
+
+	public static void main(String[] args) {
+		 
+		Scanner in = new Scanner(System.in);
+ 
+		double R = in.nextDouble();	// 반지름 R
+		in.close();
+		
+		System.out.println(R * R * Math.PI);	// 유클리드 원의 넓이
+		System.out.println(2 * R * R);		// 택시기하학 원의 넓이
+	}
+}
